a1f03253c65b94d25d666e6c1f7d1d12b59bfb1e
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-03-20 13:34:09 +0700

    Membuat Controller, key, dan variabel

--- lib/todoApp_page.dart
+++ lib/todoApp_page.dart
@@ -8,6 +8,15 @@ class TodoappPage extends StatefulWidget {
 }
 
 class _TodoappPageState extends State<TodoappPage> {
+  final TextEditingController _nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final ScrollController _scrollController = ScrollController();
+  final List<String> tasks = [];
+  final List<DateTime> taskDates = [];
+  final List<bool> _isChecked = [];
+  DateTime? _selectedDate;
+  bool _isSubmitted = false;
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
