b411a0953e072f6dd7d20335426ac46e4d363c11
nadiacahyadewi <nadiacahyadewi20@gmail.com>
2025-03-20 13:08:59 +0700

    membuat todoapp statefull widget

---
+++ lib/todoApp_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TodoappPage extends StatefulWidget {
+  const TodoappPage({super.key});
+
+  @override
+  State<TodoappPage> createState() => _TodoappPageState();
+}
+
+class _TodoappPageState extends State<TodoappPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
